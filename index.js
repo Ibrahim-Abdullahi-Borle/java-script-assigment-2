297be0bc1d8be83850f7fbf48591001f0795ed60
Ibrahim-Abdullahi-Borle <ibroabdullahi254@gmail.com>
2025-05-04 04:16:54 -0700

    java-script-assigment-2

---
+++ index.js
@@ -0,0 +1,27 @@
+// 1. Ask the user for their name
+var name = prompt("What is your name?");
+console.log(name);
+
+
+// 2. Ask for their age
+var age = prompt("How old are you?");
+console.log(age);
+
+
+// 3. Ask what their favorite color is
+var color = prompt("What is your favorite color?");
+console.log(color)
+
+
+
+// 4. Print all three inputs to the console
+var name= "IBRAHIM";
+console.log("name");
+console.log("25");
+console.log("blue");
+
+
+// 5. Print the data type of each input
+console.log( typeof "ibro");
+console.log(typeof 25);
+console.log(typeof blue)
